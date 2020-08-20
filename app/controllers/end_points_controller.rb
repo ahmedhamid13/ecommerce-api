5a9edb54f241e16a84a6a43ad6b7a47aca31c997
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-08-20 07:30:09 +0200

    change end_points host_url

--- app/controllers/end_points_controller.rb
+++ app/controllers/end_points_controller.rb
@@ -4,58 +4,58 @@ class EndPointsController < ApplicationController
       category: [
         {
           method: 'GET',
-          path: request.url + 'categories',
+          path: Rails.configuration.api_url + '/categories',
           description: ''
         },
         {
           method: 'GET',
-          path: request.url + 'categories/:id',
+          path: Rails.configuration.api_url + '/categories/:id',
           description: ''
         },
         {
           method: 'POST',
-          path: request.url + 'categories',
+          path: Rails.configuration.api_url + '/categories',
           params: 'title',
           description: ''
         },
         {
           method: 'PUT',
-          path: request.url + 'categories/:id',
+          path: Rails.configuration.api_url + '/categories/:id',
           params: 'title',
           description: ''
         },
         {
           method: 'DELETE',
-          path: request.url + 'categories/:id',
+          path: Rails.configuration.api_url + '/categories/:id',
           description: ''
         }
       ],
       item: [
         {
           method: 'GET',
-          path: request.url + 'items',
+          path: Rails.configuration.api_url + '/items',
           description: ''
         },
         {
           method: 'GET',
-          path: request.url + 'items',
+          path: Rails.configuration.api_url + '/items',
           params: 'search',
           description: ''
         },
         {
           method: 'GET',
-          path: request.url + 'items/:id',
+          path: Rails.configuration.api_url + '/items/:id',
           description: ''
         },
         {
           method: 'POST',
-          path: request.url + 'items',
+          path: Rails.configuration.api_url + '/items',
           params: 'title, brand, description, stock, price, images[], category_id, sub_categories[]',
           description: ''
         },
         {
           method: 'PUT',
-          path: request.url + 'items/:id',
+          path: Rails.configuration.api_url + '/items/:id',
           params: 'title, brand, description, stock, price, images[]',
           description: ''
         }
@@ -63,102 +63,102 @@ class EndPointsController < ApplicationController
       cart: [
         {
           method: 'GET',
-          path: request.url + 'carts',
+          path: Rails.configuration.api_url + '/carts',
           description: ''
         },
         {
           method: 'GET',
-          path: request.url + 'carts/:id',
+          path: Rails.configuration.api_url + '/carts/:id',
           description: ''
         },
         {
           method: 'POST',
-          path: request.url + 'carts',
+          path: Rails.configuration.api_url + '/carts',
           params: 'item_id, user_id, quantity',
           description: ''
         },
         {
           method: 'DELETE',
-          path: request.url + 'carts/items/:id',
+          path: Rails.configuration.api_url + '/carts/items/:id',
           description: 'Remove Item from cart'
         },
         {
           method: 'DELETE',
-          path: request.url + 'carts/:id',
+          path: Rails.configuration.api_url + '/carts/:id',
           description: ''
         }
       ],
       order: [
         {
           method: 'GET',
-          path: request.url + 'orders',
+          path: Rails.configuration.api_url + '/orders',
           description: ''
         },
         {
           method: 'GET',
-          path: request.url + 'orders/:id',
+          path: Rails.configuration.api_url + '/orders/:id',
           description: ''
         },
         {
           method: 'POST',
-          path: request.url + 'orders',
+          path: Rails.configuration.api_url + '/orders',
           params: 'user_id, country, city, address, telephone',
           description: ''
         },
         {
           method: 'PUT',
-          path: request.url + 'orders/:id',
+          path: Rails.configuration.api_url + '/orders/:id',
           params: 'country, city, address, telephone',
           description: ''
         },
         {
           method: 'PUT',
-          path: request.url + 'orders/deliver/:id',
+          path: Rails.configuration.api_url + '/orders/deliver/:id',
           description: 'Update Order state to "delivered"'
         },
         {
           method: 'DELETE',
-          path: request.url + 'orders/:id',
+          path: Rails.configuration.api_url + '/orders/:id',
           description: ''
         }
       ],
       user: [
         {
           method: 'GET',
-          path: request.url + 'users',
+          path: Rails.configuration.api_url + '/users',
           description: ''
         },
         {
           method: 'GET',
-          path: request.url + 'users/:id',
+          path: Rails.configuration.api_url + '/users/:id',
           description: ''
         },
         {
           method: 'POST',
-          path: request.url + 'users',
+          path: Rails.configuration.api_url + '/users',
           params: 'name, email, password, password_confirmation, avatar',
           description: ''
         },
         {
           method: 'PUT',
-          path: request.url + 'users',
+          path: Rails.configuration.api_url + '/users',
           params: 'password, password_confirmation',
           description: ''
         },
         {
           method: 'DELETE',
-          path: request.url + 'users',
+          path: Rails.configuration.api_url + '/users',
           description: ''
         },
         {
           method: 'POST',
-          path: request.url + 'users/sign_in',
+          path: Rails.configuration.api_url + '/users/sign_in',
           params: 'email, password',
           description: ''
         },
         {
           method: 'DELETE',
-          path: request.url + 'users/sign_out',
+          path: Rails.configuration.api_url + '/users/sign_out',
           params: 'uid, access-token, client',
           description: ''
         }
